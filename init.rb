a3d283a64f66d8ae7302b24eb492f7c19aebcb3b
Matías Flores <mflores@atlanware.com>
2008-10-24 14:42:25 -0200

    Ahora se eliminan todos los directorios temporales creados por rubyscript2exe

--- init.rb
+++ init.rb
@@ -1,12 +1,26 @@
 #
 # Web Services Facturacion Electronica AFIP
-# Copyright (C) 2008 Matiás Alejandro Flores <mflores@atlanware.com>
+# Copyright (C) 2008 Matias Alejandro Flores <mflores@atlanware.com>
 #
 $: << File.expand_path(File.dirname(__FILE__) + "/lib")
 
-require 'wsfe'
+require 'lib/wsfe'
 require 'silence'
 require 'rubyscript2exe'
+
+if RUBYSCRIPT2EXE.is_compiled?
+  ee_base_folder = File.expand_path(RUBYSCRIPT2EXE.appdir + "/../..")
+  ee_folder = File.expand_path(RUBYSCRIPT2EXE.appdir + "/..")
+  name_parts =  ee_folder.split(".")
+  curr_ee_folder_number = name_parts[3].to_i
+  last_ee_folder_number = curr_ee_folder_number - 1
+  Dir.entries(ee_base_folder).each do |folder|
+    if (folder != ".") and (folder != "..") and (folder.split(".")[3].to_i != curr_ee_folder_number)
+      folder_to_del = ee_base_folder + "/" + folder
+      FileUtils.remove_dir(folder_to_del, true) 
+    end
+  end
+end
 
 silence_warnings do
   WSFE::Runner::Wsfe.run(ARGV)
