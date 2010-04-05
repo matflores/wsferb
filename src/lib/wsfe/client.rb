8f15af5a3fcf8e3b039f73923b57d63978151f99
Matías Flores <mflores@atlanware.com>
2010-04-05 13:24:54 -0300

    Updating copyright notice

--- src/lib/wsfe/client.rb
+++ src/lib/wsfe/client.rb
@@ -1,6 +1,6 @@
 #
 # Web Services Facturacion Electronica AFIP
-# Copyright (C) 2008 Matias Alejandro Flores <mflores@atlanware.com>
+# Copyright (C) 2008-2010 Matias Alejandro Flores <mflores@atlanware.com>
 #
 require 'time'
 require 'soap/wsdlDriver'
