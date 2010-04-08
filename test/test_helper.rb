bd188985bf8c8d9abeffac04ca67af6f7fe97ce0
Matías Flores <mflores@atlanware.com>
2010-04-08 15:40:43 -0300

    Silence warnings in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ $: << File.expand_path(File.dirname(__FILE__) + "/../src/lib")
 
 require 'rubygems'
 require 'protest'
+require 'silence'
 require 'wsaa'
 require 'wsfe'
 require 'wsfex'
@@ -13,4 +14,11 @@ WSFEX::Client.enable_test_mode
 CERT_FILE = File.join File.dirname(__FILE__), 'credentials', '20238883890.crt'
 KEY_FILE  = File.join File.dirname(__FILE__), 'credentials', '20238883890.key'
 
+Protest.autorun = false
 Protest.report_with(:documentation)
+
+at_exit do
+  silence_warnings do
+    Protest.run_all_tests!
+  end
+end
