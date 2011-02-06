cb6d1a2a6c742ffbb679b7be8398e87864772ddd
Matías Flores <mflores@atlanware.com>
2011-02-06 11:05:08 -0300

    Add tests for most WSFE methods

--- test/integration/test_wsfe_client.rb
+++ test/integration/test_wsfe_client.rb
@@ -1,46 +1,79 @@
 require 'test_helper'
 
-Protest.describe "WSFE client" do
-  it "can test if all systems are ok" do
-    assert_equal "authserver=OK; appserver=OK; dbserver=OK;", WSFE::Client.test
-  end
-
-  describe "given a valid ticket" do
-    before do
-      cert_file = File.dirname(__FILE__) + "/../credentials/20238883890.crt"
-      key_file = File.dirname(__FILE__) + "/../credentials/20238883890.key"
-      @valid_ticket = WSAA::Client.requestTicket(20238883890, 'wsfe', cert_file, key_file)
-
-      assert @valid_ticket.valid?
-    end
-
-    it "returns the maximum number of records to be included in the wsfe FEAutorizarRequest service" do
-      response = WSFE::Client.recuperaMaxQty(@valid_ticket)
-      assert_equal 250, response.value
-      assert_equal 0, response.errCode
-      assert_equal 'OK', response.errMsg
-    end
-
-    it "returns the last transaction number" do
-      response = WSFE::Client.recuperaUltNroTransaccion(@valid_ticket)
-
-      assert_not_equal 0, response.value
-      assert_equal 0, response.errCode
-      assert_equal 'OK', response.errMsg
+class Protest::TestCase
+  def self.test_method(method, arguments = nil, send_credentials = true)
+    test method do
+      execute method, arguments, send_credentials
+      check_output method
     end
   end
+end
 
-  describe "given an invalid ticket" do
+Protest.describe "WSFE" do
+  test_method :FEDummy, '', false
+
+  test :FEUltNroRequest do
+    execute :FEUltNroRequest
+    assert_value :FEUltNroRequest, 1, 10000000000
+  end
+
+  test :FERecuperaLastCMPRequest do
+    execute :FERecuperaLastCMPRequest, "01 0001"
+    assert_value :FERecuperaLastCMPRequest, 1, 1000
+  end
+
+  test :FERecuperaQTYRequest do
+    execute :FERecuperaQTYRequest
+    assert_value :FERecuperaQTYRequest, 250
+  end
+
+  test :FEConsultaCAERequest do
+    execute :FEConsultaCAERequest, "10 20238883890 01 0001 00000001 1210.0 20110101"
+    assert_value :FEConsultaCAERequest, 0
+  end
+
+  describe "FEAutRequest" do
     before do
-      @invalid_ticket = WSAA::Ticket.new(:cuit => 20238883890)
-      assert !@invalid_ticket.valid?
-    end
 
-    it "returns an error when requesting the maximum number of records" do
-      response = WSFE::Client.recuperaMaxQty(@invalid_ticket)
-
-      assert_equal 1000, response.errCode
-      assert_not_equal 'OK', response.errMsg
     end
+    it "success" do
+    end
+  end
+
+  def execute(method, arguments = nil, send_credentials = true)
+    `wsfe #{method} #{arguments} #{credentials if send_credentials} --test > test/output/#{method}.txt`
+  end
+
+  def credentials
+    "--cuit 20238883890 --cert test/credentials/20238883890.crt --key test/credentials/20238883890.key"
+  end
+
+  def check_output(method)
+    sample_file = expand_path("samples/#{method}.txt")
+    output_file = expand_path("output/#{method}.txt")
+
+    assert_equal '', `diff #{sample_file} #{output_file}`
+    assert $?.success?, 'diff failed'
+  end
+
+  def expand_path(filename)
+    File.join(File.dirname(__FILE__), '..', filename)
+  end
+
+  def assert_value(method, min, max = nil)
+    max ||= min
+
+    output = File.read(expand_path("output/#{method}.txt"))
+    response = AFIP::Response.parse(output)
+
+    if numeric?(response.value)
+      assert response.value.to_f >= min && response.value.to_f <= max, "wrong value (#{response.value})"
+    else
+      assert response.value == min, "wrong value (#{response.value})"
+    end
+  end
+
+  def numeric?(value)
+    true if Float(value) rescue false
   end
 end
