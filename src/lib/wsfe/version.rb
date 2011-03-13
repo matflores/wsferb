6eb21a16e65920feba06c6178c73b7171114fb27
Matías Flores <mflores@atlanware.com>
2011-03-13 16:25:25 -0300

    Update version

--- src/lib/wsfe/version.rb
+++ src/lib/wsfe/version.rb
@@ -5,13 +5,12 @@
 module WSFE
   module VERSION
     unless defined? MAJOR
-      MAJOR  = 1
-      MINOR  = 1
-      TINY   = 5
+      MAJOR  = 2
+      MINOR  = 0
+      TINY   = 0
       RELEASE_CANDIDATE = nil
 
       STRING = [MAJOR, MINOR, TINY].join('.')
-      TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
       FULL_VERSION = "#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')}"
 
       NAME   = "wsfe"
