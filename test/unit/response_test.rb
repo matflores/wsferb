d449482698461b6f8ff5eb85933b2db49687ea96
Matías Flores <mflores@atlanware.com>
2011-03-13 15:23:34 -0300

    Fix response tests

--- test/unit/response_test.rb
+++ test/unit/response_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 Protest.describe "A response" do
   it "should be successful if the error code is 0" do
     response = AFIP::Response.new
-    response.errCode = 0
+    response.err_code = 0
 
     assert response.success?
   end
 
   it "should not be successful if error code is different than 0" do
     response = AFIP::Response.new
-    response.errCode = 1000
+    response.err_code = 1000
 
     assert !response.success?
   end
@@ -18,8 +18,8 @@ Protest.describe "A response" do
   it "should include value, error code and error message in the string representation" do
     response = AFIP::Response.new
     response.value   = 0
-    response.errCode = 1000
-    response.errMsg  = "Not authorized"
+    response.err_code = 1000
+    response.err_msg  = "Not authorized"
 
     assert_equal "[Respuesta]\nvalor=0\nerrCode=1000\nerrMsg=Not authorized", response.to_s
   end
@@ -28,7 +28,7 @@ Protest.describe "A response" do
     response = AFIP::Response.parse("[Respuesta]\nvalor=0\nerrCode=1000\nerrMsg=Not authorized")
 
     assert_equal "0", response.value
-    assert_equal "1000", response.errCode
-    assert_equal "Not authorized", response.errMsg
+    assert_equal "1000", response.err_code
+    assert_equal "Not authorized", response.err_msg
   end
 end
