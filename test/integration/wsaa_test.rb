4906d5e6a40ab8abcfc12f47af5abfe7098d0e8e
Matías Flores <mflores@atlanware.com>
2011-04-20 10:28:37 -0300

    Test requesting a ticket for WSFEX as well as WSFE

--- test/integration/wsaa_test.rb
+++ test/integration/wsaa_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 Protest.describe "WSAA client" do 
-  it "should return a valid ticket for wsfe" do
-    ticket = WSAA::Client.requestTicket(CUIT, 'wsfe', CERT_FILE, KEY_FILE)
+  %w(wsfe wsfex).each do |service|
+    it "should return a valid ticket for #{service}" do
+      ticket = WSAA::Client.requestTicket(CUIT, service, CERT_FILE, KEY_FILE)
 
-    assert ticket.valid?
+      assert ticket.valid?
+    end
   end
 end
