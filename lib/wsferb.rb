e85c1c5a3eb7d428d61402f719ee45c3175d824a
Matías Flores <mflores@atlanware.com>
2011-04-23 18:23:55 -0300

    Create a top-level module and move all submodules inside the new module

---
+++ lib/wsferb.rb
@@ -0,0 +1,12 @@
+#
+# Web Services Facturacion Electronica AFIP
+# Copyright (C) 2008-2011 Matias Alejandro Flores <mflores@atlanware.com>
+#
+require "core_ext/hash"
+require "core_ext/kernel"
+require "wsferb/wsaa"
+require "wsferb/wsfe"
+require "wsferb/wsfex"
+
+module WSFErb
+end
