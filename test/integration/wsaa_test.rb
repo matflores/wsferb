84b28edc5f760b7b07a82a8b7f61822c99062a48
Matías Flores <mflores@atlanware.com>
2011-04-30 09:15:20 -0300

    Fix WSAA tests

--- test/integration/wsaa_test.rb
+++ test/integration/wsaa_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 Protest.describe "WSAA client" do 
   %w(wsfe wsfex).each do |service|
     it "should return a valid ticket for #{service}" do
-      ticket = WSAA::Client.requestTicket(CUIT, service, CERT_FILE, KEY_FILE)
+      ticket = WSFErb::WSAA::Client.requestTicket(CUIT, service, CERT_FILE, KEY_FILE)
 
       assert ticket.valid?
     end
