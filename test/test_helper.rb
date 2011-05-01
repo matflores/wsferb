ba820bcdf063e3d683579f25248ee7435024e717
Matías Flores <mflores@atlanware.com>
2011-05-01 17:14:57 -0300

    Extract common test helper methods to test_helper module

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -95,6 +95,47 @@ class Protest::TestCase
   def script
     "wsfe"
   end
+
+  def self.test_common_errors(service = nil, args = nil)
+    if service
+      test "Missing CUIT" do
+        execute service, args, "--cert #{CERT_FILE} --key #{KEY_FILE}"
+        assert_error service, 900002, "CUIT no informado"
+      end
+
+      test "Certificate not found" do
+        execute service, args, "--cuit 20238883890 --cert unknown.crt --key #{KEY_FILE}"
+        assert_error service, 900003, "Certificado no encontrado: #{File.expand_path('unknown.crt')}"
+      end
+
+      test "Private key not found" do
+        execute service, args, "--cuit 20238883890 --cert #{CERT_FILE} --key unknown.key"
+        assert_error service, 900004, "Clave privada no encontrada: #{File.expand_path('unknown.key')}"
+      end
+
+      test "Invalid output dir" do
+        execute service, "--out unknown_dir/output.txt"
+        assert_error service, 900006, "Directorio no valido: #{File.expand_path('unknown_dir')}"
+      end
+
+      test "Invalid log dir" do
+        execute service, "--log unknown_dir/output.log"
+        assert_error service, 900006, "Directorio no valido: #{File.expand_path('unknown_dir')}"
+      end
+    else
+      describe "common errors" do
+        test "Invalid Service (blank)" do
+          execute ""
+          assert_error "", 900001, "Servicio no informado"
+        end
+
+        test "Invalid Service (unknown)" do
+          execute :Unknown
+          assert_error :Unknown, 900001, "Servicio no valido: Unknown"
+        end
+      end
+    end
+  end
 end
 
 Protest.autorun = false
