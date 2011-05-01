85646aa1d527707ee20e5db8665a8c4562fa8469
Matías Flores <mflores@atlanware.com>
2011-05-01 17:58:17 -0300

    Fix FECompUltimoAutorizado

--- lib/wsferb/wsfe/response/feCompUltimoAutorizado.rb
+++ lib/wsferb/wsfe/response/feCompUltimoAutorizado.rb
@@ -4,20 +4,12 @@
 module WSFErb
   module WSFE
     class Response::FECompUltimoAutorizado < Response
-      def formatted_records
-        records.map { |record| format_record(record) }
-      end
-
       def format_record(record)
-        [ "1%03d%04d%08d" % [ result[:cbte_tipo], result[:pto_vta], result[:cbte_nro] ] ]
+        "1%03d%04d%08d" % [ result[:cbte_tipo], result[:pto_vta], result[:cbte_nro] ]
       end
 
       def records
-        @records ||= [result]
-      end
-
-      def value
-        result[:cbte_nro] rescue "n/d"
+        @records ||= [ result ]
       end
 
       def result
