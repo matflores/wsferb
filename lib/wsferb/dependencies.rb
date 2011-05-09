b5103dc83ada82b3a7a66317573b01f7c717ed1a
Matías Flores <mflores@atlanware.com>
2011-05-09 09:22:24 -0300

    Explicitely require dependencies so that OCRA includes them in the generated exes

---
+++ lib/wsferb/dependencies.rb
@@ -0,0 +1,11 @@
+#
+# Web Services Facturacion Electronica AFIP
+# Copyright (C) 2008-2011 Matias Alejandro Flores <mflores@atlanware.com>
+#
+# Any files required here will be included by OCRA (one-click ruby application)
+
+require "savon"
+require "httpi"
+require "httpclient"
+require "bigdecimal"
+require "syck"
